cbb195a344b4226e457e6303ef77efb3234382fb
SU-I-KA <Bmagdy@mail.com>
2022-05-29 08:53:38 +0300

    render msg when no posts

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -2,6 +2,13 @@ import React from 'react'
 import Post from '../Post/Post'
 import Banner from '../Banner/Banner'
 const Posts = ({ posts, title }) => {
+  if (posts.length === 0) {
+    return (
+      <section className='posts'>
+        <h3 className='posts-title'>'There are no posts yet'</h3>
+      </section>
+    )
+  }
   return (
     <section className='posts'>
       <h3 className='posts-title'>{title}</h3>
